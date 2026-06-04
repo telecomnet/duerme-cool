119425e6c345b6a0c185a30c65d3e417fa3e159b
telecomnet <60301817+telecomnet@users.noreply.github.com>
2026-06-04 09:40:23 -0600

    Brand restyle: load Jost+Inter fonts, Tailwind font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        display: ['Jost', 'Inter', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
